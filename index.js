bbbedebb7140d49065f6e150675a79b38518e3df
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2023-05-09 15:31:45 +1200

    Add bootstrap row and cols; change card text colors

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ const imagesUrl = "https://storage.googleapis.com/luxe_media/wwwroot/"
 const container = document.getElementById("product-container")
 
 products.forEach((product) => {
-  const productElement = document.createElement("a")
+  const productElement = document.createElement("div")
+
+  productElement.className = "col"
 
   const productImage = `<img src="${
     imagesUrl + product.productMedia[0].url
   }" alt="" class="product-image"/>`
   const productTitle = `<h2 class="product-title">${product.title}</h2>`
-  const productPrice = `<p>$ ${product.price}</p>`
+  const productPrice = `<p class="product-price">$ ${product.price}</p>`
   const productContent = `<div class="product-content">${
     productTitle + productPrice
   }</div>`
-  productElement.setAttribute("href", `./${makeSlug(product.title)}`)
-  productElement.className = "product-card"
+  const productCard = `<a class="product-card" href="./${makeSlug(
+    product.title
+  )}">${productImage + productContent}</a>`
 
-  productElement.innerHTML = productImage + productContent
+  productElement.innerHTML = productCard
   container.appendChild(productElement)
 })
